f059c575d2c3b1ddaf78bebf73b5c7da47685823
AnLong <alon@cladonia.team>
2023-11-04 10:10:20 +0800

    Add relevant comments for easy reference (zh-cn)

--- System/clock.js
+++ System/clock.js
@@ -1,3 +1,8 @@
+//获取系统时间并显示
+
+//使用方法：在 HTML 的 <body onload="load();"> 标签内使用 <script> 引用，或是将以下所有代码全部复制到 HTML 内需要显示时间的 <script< >元素标签内。
+
+//获取时间
 Date.prototype.format = function (fmt) {
     var o = {
         "y+": this.getFullYear, //年
@@ -12,5 +17,16 @@ Date.prototype.format = function (fmt) {
         if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
 }
-setInterval("document.getElementById('date').innerHTML = (new Date()).format('yyyy 年 MM 月 dd 日');", 60000);
-setInterval("document.getElementById('clock').innerHTML = (new Date()).format('hh:mm');", 100);
+
+//引用并显示时间
+
+//在 HTML 需要显示时间的标签上添加 id=""，如引用yyyy-mm-dd标签，就添加 id="yyyy-mm-dd"
+
+//日期（yyyy-mm-dd）
+setInterval("document.getElementById('yyyy-mm-dd').innerHTML = (new Date()).format('yyyy 年 MM 月 dd 日');", 60000);
+//日期（mm-dd-yyyy）
+setInterval("document.getElementById('mm-dd-yyyy').innerHTML = (new Date()).format('MM 月 dd 日，yyyy年');", 60000);
+//时间（hh-mm）
+setInterval("document.getElementById('hh-mm').innerHTML = (new Date()).format('hh:mm');", 100);
+//时间（hh-mm-ss）
+setInterval("document.getElementById('hh-mm-ss').innerHTML = (new Date()).format('hh:mm:ss');", 10
